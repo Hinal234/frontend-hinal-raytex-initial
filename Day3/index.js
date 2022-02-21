b5ccb0ee8cc0b7a754b002fdef79fb32d50a9c67
Hinal234 <hinalhapani234@gmail.com>
2022-02-21 10:51:17 +0530

    Day 3 Task1 Added

---
+++ Day3/index.js
@@ -0,0 +1,52 @@
+var name = "James";
+var numberone = 1;
+var verify = true;
+
+var numbertwo = 2;
+//var sum = numberone + numbertwo;
+//var mul = numberone * numbertwo;
+
+//ES 6 let
+
+let fname="Thomas";
+
+
+//Object
+
+const student = {name:"James", Age:20};
+
+//document.getElementById("valobject").innerHTML = student.name + "  "+ student.Age;
+
+
+function displayobject(){
+    //document.getElementById("valobject").innerHTML = this.student.name + this.student.Age;
+    return student.name + student.Age;
+}
+
+
+function addition(){
+
+    var sum = numberone + numbertwo;
+    var mul = numberone * numbertwo;
+
+    document.getElementById("newone").innerHTML="Author";
+    document.getElementById("newtwo").innerHTML=sum;
+    document.getElementById("newthree").innerHTML=mul;
+
+    console.log(sum);
+    console.log(mul);
+
+    return sum;
+}
+
+
+
+function checkEven(){
+    var picknumb = document.getElementById("readnum").value;
+    
+    if(picknumb%2 ==0){
+        console.log("Even");
+    }else{
+        console.log("Odd");
+    }
+}
